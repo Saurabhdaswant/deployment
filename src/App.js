a9d2413a5db06ed5417379a9defbf80ee9461c69
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-18 19:01:29 +0530

    new start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+// import { useState } from "react";
+
+import NavBar from "./NavBar";
+
 function App() {
   return (
     <div className="App">
       <h1>what the fuck man</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <h1>this is my new blog bro</h1>;
+};
+
+export default NavBar;
