5b53c93c052ad6dded3320e779a4fd208651799d
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-27 16:41:18 +0530

    fetching data and handling error msg

--- src/App.js
+++ src/App.js
@@ -1,87 +1,40 @@
-// import { useState } from "react";
-
 import NavBar from "./NavBar";
 import Blog from "./Blog";
-
-//the database
-const dataBase = [
-  {
-    name: " saurabh daswant",
-    topic: " how to learn web dev in 6 months",
-    date: " jun 16, 2021",
-    domain: "blog.saurabh.com",
-    context:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when",
-    like: 55,
-    feed: "recent",
-    id: "1",
-  },
-  {
-    name: " sahil kadolkar",
-    topic: "lets talk about my 5 gf",
-    date: " jun 15, 2021",
-    domain: "blog.sahil.com",
-    context:
-      " use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in thei",
-    like: 27,
-    feed: "featured",
-    id: "2",
-  },
-  {
-    name: " yash bisale ",
-    topic: " bhosdicha warren buffet",
-    date: " jun 30, 2021",
-    domain: "blog.yash.com",
-    context:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when",
-    like: 32,
-    feed: "relevant",
-    id: "3",
-  },
-  {
-    name: " sammer gaikwad ",
-    topic: " introduction into the pubg environment  ",
-    date: " may 1, 2021",
-    domain: "blog.kalu.com",
-    context:
-      "ok even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the",
-    like: 21,
-    feed: "recent",
-    id: "4",
-  },
-  {
-    name: " atharva dhapal ",
-    topic: " Are you getting less marks in online exam?",
-    date: " nov 6, 2021",
-    domain: "blog.pathatshir.com",
-    context:
-      "how to copy and paste the worlds best tip you will get hreereadable content of a page when",
-    like: 200,
-    feed: "relevant",
-    id: "5",
-  },
-];
+import { useEffect, useState } from "react";
 
 function App() {
+  const [post, setPost] = useState(null);
+  const [error, setError] = useState(null);
+  const [pending, setPending] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:7000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("you database dosent have data madar fakir");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setPending(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
-      {dataBase.map((data) => {
-        return (
-          <div>
-            <Blog
-              key={data.id}
-              {...data}
-              // name={data.name}
-              // date={data.date}
-              // topic={data.topic}
-              // domain={data.domain}
-              // context={data.context}
-              // like={data.like}
-            />
-          </div>
-        );
-      })}
+      {pending && <div>data is coming soon</div>}
+      {error && <div>{error}</div>}
+      {post &&
+        post.map((data) => {
+          return <div>{<Blog key={data.id} {...data} />}</div>;
+        })}
     </div>
   );
 }
